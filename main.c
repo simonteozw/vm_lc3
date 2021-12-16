73e2d882933e9ea8fb0dfc8fb1e292155cd350d4
simonteozw <simonteozw@gmail.com>
2021-12-17 04:42:26 +0800

    Add enums

--- main.c
+++ main.c
@@ -5,3 +5,54 @@
 #include <unistd.h>
 #include <sys/termios.h>
 #include <signal.h>
+
+// LC-3 has 2^16 memory locations
+uint16_t memory[UINT16_MAX]
+
+// 8 general purpose registers, 1 PC, 1 conditional flag
+enum {
+    R_R0 = 0,
+    R_R1,
+    R_R2,
+    R_R3,
+    R_R4,
+    R_R5,
+    R_R6,
+    R_R7,
+    R_PC,
+    R_COND,
+    R_COUNT
+}
+
+uint16_t reg[R_COUNT]
+
+enum {
+    OP_BR = 0, // branch
+    OP_ADD,    // add
+    OP_LD,     // load
+    OP_ST,     // store
+    OP_JSR,    // jump register
+    OP_AND,    // bitwise and
+    OP_LDR,    // load register
+    OP_STR,    // store register
+    OP_RTI,    // unused
+    OP_NOT,    // bitwise not
+    OP_LDI,    // load indirect
+    OP_STI,    // store indirect
+    OP_JMP,    // jump
+    OP_RES,    // unused
+    OP_LEA,    // load effective address
+    OP_TRAP    // execute trap
+}
+
+enum {
+    FL_POS = 1 << 0, // positive
+    FL_ZRO = 1 << 1, // zero
+    FL_NEG = 1 << 2  // negative
+}
+
+int main() {
+    // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
+}
