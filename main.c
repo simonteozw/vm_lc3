a8b394bf21da1f5e752c639682cf25bbf5ddace9
simonteozw <simonteozw@gmail.com>
2021-12-17 11:25:40 +0800

    Add sign_extend and update_flags helper functions

--- main.c
+++ main.c
@@ -51,8 +51,60 @@ enum {
     FL_NEG = 1 << 2  // negative
 }
 
-int main() {
-    // printf() displays the string inside quotation
-   printf("Hello, World!");
-   return 0;
+int main(int argc, const char* argv[]) {
+
+    if (argc < 2) {
+        /* show usage string */
+        printf("./main [image-file1] ...\n");
+        exit(2);
+    }
+    for (int j = 1; j < argc; j++) {
+        if (!read_image(argv[j])) {
+            printf("failed to load image: %s\n", argv[j]);
+            exit(1);
+        }
+    }
+
+    enum { PC_START = 0x3000 };
+    reg[R_PC] = PC_START
+
+    uint16_t sign_extend(uint16_t x, int bitcount) {
+        // if x is negative add ones (using 2s complement)
+        if ((x >> bitcount - 1) & 1) x |= (0xFFFF << bitcount);
+        return x;
+    }
+
+    void update_flags(uint16_t r) {
+        if (reg[r] == 0) reg[R_COND] = FL_ZRO;
+        else if (reg[r] >> 15) reg[R_COND] = FL_NEG; // 1 in left most bit means -ve
+        else reg[R_COND] = FL_POS;
+    }
+
+    int running = 1;
+    while (running) {
+        uint16_t instr = mem_read(reg[R_PC]);
+        reg[R_PC]++;
+        uint16_t op = instr >> 12;
+
+        switch (op) {
+            case OP_BR:
+            case OP_ADD:
+            case OP_LD:
+            case OP_ST:
+            case OP_JSR:
+            case OP_AND:
+            case OP_LDR:
+            case OP_STR:
+            case OP_RTI:
+            case OP_NOT:
+            case OP_LDI:
+            case OP_STI:
+            case OP_JMP:
+            case OP_RES:
+            case OP_LEA:
+            case OP_TRAP:
+            default:
+                break;
+        }
+    }
 }
